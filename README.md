230d4e90836663cd1acb3e7a5d348ccd689e7d93
Miguel Correia <geral@nologic.pt>
2023-12-01 09:53:54 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nologic_tasks_base
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/nologic_tasks_base/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.nologic_tasks_base
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
